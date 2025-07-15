8ac08e1dc04d63fe00752a85661b93c6be637c0e
Tonycs <tonycs1247@gmail.com>
2025-07-14 23:21:40 -0400

    gave the cells instructions + added a pause and reset key

--- gen.py
+++ gen.py
@@ -1,8 +1,7 @@
 import random
-import pygame
 import math
 
-WIDTH, HEIGHT = 1000, 800
+WIDTH, HEIGHT = 1000, 800  # size of the pygame window
 RESOLUTION = 20  # this constant represents the size of the cells within the grid
 grid = []
 columns, rows = 0, 0  # placeholder values
@@ -10,6 +9,7 @@ columns, rows = 0, 0  # placeholder values
 def make2DArray():  # creates a 2D array which will represent the grid
     global columns
     global rows
+    global grid
 
     columns = round(WIDTH / RESOLUTION)
     rows = round(HEIGHT / RESOLUTION)
@@ -17,7 +17,34 @@ def make2DArray():  # creates a 2D array which will represent the grid
     for i in range(columns):
         grid.append([])
         for j in range(rows):
-            grid[i].append([])
-            grid[i][j].append(math.floor(random.random() * 1.2))  # randomly assigns a state to each cell in the grid
+            # randomly assigns a state to each cell in the grid
+            # the multiplier determines the amount of live cells there are -> higher chance of 1's
+            grid[i].append(math.floor(random.random() * 1.1))
 
     return grid
+
+# iterates through the eight surrounding cells of a singular cell & counts the amount of alive cells
+def countNeighbors(cellColumn, cellRow, currentState): 
+    count = 0
+
+    count += grid[cellColumn - 1][cellRow - 1] + grid[cellColumn - 1][cellRow] + grid[cellColumn - 1][cellRow + 1]
+    count += grid[cellColumn][cellRow - 1] + grid[cellColumn][cellRow + 1]
+    count += grid[cellColumn + 1][cellRow - 1] + grid[cellColumn + 1][cellRow] + grid[cellColumn + 1][cellRow + 1]
+
+    if currentState == 0 and count == 3:
+        currentState = 1
+    elif currentState == 1 and (count < 2 or count > 3):
+        currentState = 0
+
+    return currentState
+
+def updateState():  # updates the state of individual cells based on its surrounding cells
+    global grid
+
+    for i in range(1, columns - 1):  # the altered range is to prevent calculation on border cells
+        for j in range(1, rows - 1):
+            grid[i][j] = countNeighbors(i, j, grid[i][j])
+    
+    return grid
+
+
--- main.py
+++ main.py
@@ -10,23 +10,35 @@ pygame.display.set_caption("Conway's Game of Life Sim")
 
 def main():
     running = True
+    genFlag = True
     clock = pygame.time.Clock()
     gen.make2DArray()
 
     while running:
         WINDOW.fill((0,0,0))
-        clock.tick(20)
+        clock.tick(10)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    gen.grid = []
+                    gen.make2DArray()
+                elif event.key == pygame.K_SPACE and genFlag:
+                    genFlag = False
+                elif event.key == pygame.K_SPACE and not genFlag:
+                    genFlag = True
 
-        for i in range(gen.columns):
-            for j in range(gen.rows):
-                if gen.grid[i][j] == [1]:
+        for i in range(1, gen.columns - 1):
+            for j in range(1, gen.rows - 1):
+                if gen.grid[i][j] == 1:
                     x = i * gen.RESOLUTION
                     y = j * gen.RESOLUTION
                     pygame.draw.rect(WINDOW, (255, 255, 255), (x, y, gen.RESOLUTION, gen.RESOLUTION))
+        
+        if genFlag:
+            gen.updateState()
 
         pygame.display.update()
 
